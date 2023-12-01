2dcfd4aa0327efd75201495c9eaa7771d6df448f
raysamoura <mouraraysa081@gmail.com>
2023-12-01 17:29:05 -0300

    atividade 1

---
+++ atividade.js
@@ -0,0 +1,17 @@
+function mostrarCabecalho(){
+    document.write("titulo")
+}
+
+
+function mostrarCabecalho2(texto,texto2){
+    document.write(texto, texto2)
+} 
+ 
+var nome = "vitor"
+var nome1 = "Daniel"
+var nome2 = "Fabiana"
+
+mostrarCabecalho2(nome,nome2);
+mostrarCabecalho2(nome1);
+mostrarCabecalho2(nome2);
+
